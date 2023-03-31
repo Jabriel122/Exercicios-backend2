f199a73a45b579b6d6d810617ac8ae539f45d600
Gabriel Marchetti dee Souza <gbielmarch@gmail.com>
2023-03-31 10:57:29 -0300

    terminando

---
+++ 1 - Datas/Program.cs
@@ -0,0 +1,39 @@
+﻿// Programa para validar datas de aniversários 
+
+Console.WriteLine($"Bem Vindo. Digite o ano que você nasceu");
+int ano = int.Parse(Console.ReadLine());
+
+if(ano <= 2013 && ano >= 1930)
+{
+    Console.WriteLine($"Agora me fala o mês que nasceu");
+    int mes = int.Parse(Console.ReadLine());
+
+    if (mes <= 12 && mes > 0)
+    {
+        Console.WriteLine($"Agora me fala o dia que nasceu");
+        int dia = int.Parse(Console.ReadLine());
+
+        if (dia <= 31 && dia > 0)
+        {
+            int idade = 2013 - ano;
+
+            Console.WriteLine($"Você nasceu no dia {dia}, mês {mes} no de {ano}. Logo você tem mais ou menos {idade} anos");
+        }
+        else
+        {
+            Console.WriteLine($"dia inválido");
+        }
+        
+    }
+    
+    else
+    {
+        Console.WriteLine($"Mes inválido");  
+    }
+    
+}
+else
+{
+    Console.WriteLine($"Ano invalido. digite um ano válido");
+
+}
---
+++ 2 - Maior numeros entre eles/Program.cs
@@ -0,0 +1,41 @@
+﻿// Programa para medir qual numero é maior que o outro
+
+Console.WriteLine($"Bem vindo, vamos começar. digite um numero");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite outro numero, naõ pode ser igual");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Digite outro numero, lmebramdo, não pode ser igual");
+int num3 = int.Parse(Console.ReadLine());
+
+if(num1 > num2 && num2 > num3)
+{
+    Console.WriteLine($" O numero {num1} é maior que o {num2} e o mesmo é maior que o numero {num3}. Logo entre eles, o {num1} é o maior");
+}
+else if ( num2 > num1 && num1 > num3)
+{
+    Console.WriteLine($"O numero {num2} é maior que o {num1} e o mesmo é maior que o numero {num3}. Logo entre eles, o {num2} é o maior");
+}
+
+else if( num3 > num1 && num1 > num2)
+{
+    Console.WriteLine($"O numero {num3} é maior que o {num1} e o mesmo é maior que o numero {num2}. Logo entre eles, o {num3} é o maior");
+}
+
+else if ( num1 > num3 && num3 > num2)
+{
+    Console.WriteLine($"O numero {num1} é maior que o {num3} e o mesmo é maior que o numero {num2}. Logo entre eles, o {num1} é o maior");
+    
+}
+else if(num3 > num2 && num2 > num1)
+{
+    Console.WriteLine($"O numero {num3} é maior que o {num2} e o mesmo é maior que o numero {num1}. Logo entre eles, o {num3} é o maior");
+    
+}
+
+else
+{
+    Console.WriteLine($"O numero {num2} é maior que o {num3} e o mesmo é maior que o numero {num1}. Logo entre eles, o {num1} é o maior");
+    
+}
